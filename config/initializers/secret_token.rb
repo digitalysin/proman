10bda392781b4f152e50da9c52de05a913a640a3
John Doe <digitalysin@yahoo.co.id>
2012-02-01 09:32:34 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proman::Application.config.secret_token = 'dbf5cc8e74e0a2e3c01f1a73ae602dbf66580dcac80519d34c8e955efbe8b059476868b3b5c8cefda91440f7add852fd8072a64bfb9686883b6af711378afb57'
